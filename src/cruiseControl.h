5db3389459ead792bb4ec5bf123bf5062bb42dfb
David <dsu298@aucklanduni.ac.nz>
2024-06-01 20:51:47 +1200

    resolve conflcts

---
+++ src/cruiseControl.h
@@ -0,0 +1,17 @@
+// The type 'string' is defined as 'char*' in Esterel, hence passing a string by referenc  is 'char**'
+// Aviod naming a function 'recv'
+#include <stdbool.h>
+
+#define SPEED_MIN 30.0  // Minimum cruise control speed, in km/h
+#define SPEED_MAX 150.0 // Maximum cruise control speed, in km/h
+#define SPEED_INC 2.5   // Increment to change speed by when QuickAccel or QuickDecel is pressed, km/h
+#define PEDALS_MIN 3.0  // Minimum throttle and brake pedal position to trigger a change, in percent
+
+// Cruise control states
+#define OFF 0
+#define ON 1
+#define STDBY 2
+#define DISABLE 3
+
+float saturateThrottle(float throttleIn, bool *saturate);
+float regulateThrottle(bool isGoingOn, float cruiseSpeed, float vehicleSpeed);
